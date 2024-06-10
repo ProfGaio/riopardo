0d9ae0fdba063cd3efa646c8f5579420b958f573
Gaio B. Oliveira <gaio.oliveira3@etec.sp.gov.br>
2024-06-10 11:07:06 -0300

    Commit de teste

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function calcular(){
+    var estudante = document.querySelector("#estudante")
+    var nota1 = parseFloat(document.querySelector("#nota1").value)
+    var nota2 = parseFloat(document.querySelector("#nota2").value)
+    var nota3 = parseFloat(document.querySelector("#nota3").value)
+    var media = (nota1 + nota2 + nota3)/3
+    document.querySelector("#resultado").value = media.toFixed(1)
+
+}
+
